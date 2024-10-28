10607ecebe6b03ef00fbb73c8b6efb8494e95f66
etd93 <etd93@naver.com>
2024-10-28 18:39:03 +0900

    feat : Account 클래스 잔고 반환 메서드 구현

--- src/main/java/com/tdd/practice/domain/Account.java
+++ src/main/java/com/tdd/practice/domain/Account.java
@@ -2,5 +2,15 @@ package com.tdd.practice.domain;
 
 public class Account {
 
+    private int money;
+
     public Account(){}
+
+    public Account(int money){
+        this.money = money;
+    }
+
+    public int getBalance() {
+        return money;
+    }
 }
