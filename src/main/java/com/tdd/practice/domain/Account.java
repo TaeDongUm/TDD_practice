ccee7e8e5e261f88d4835a594f5b02aa3de02188
etd93 <etd93@naver.com>
2024-10-28 18:34:55 +0900

    test : Account 클래스 생성하여 성공하는 테스트로 변경

---
+++ src/main/java/com/tdd/practice/domain/Account.java
@@ -0,0 +1,6 @@
+package com.tdd.practice.domain;
+
+public class Account {
+
+    public Account(){}
+}
--- src/test/java/com/tdd/practice/domain/AccountTest.java
+++ src/test/java/com/tdd/practice/domain/AccountTest.java
@@ -1,11 +1,15 @@
 package com.tdd.practice.domain;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
 public class AccountTest {
 
+    @Test
     public void testAccount(){
         Account account = new Account();
-        if(account == null){
-            throw new IllegalArgumentException("계좌 생성이 실패했습니다.");
-        }
+        assertNotNull(account,"계좌 생성이 실패했습니다.");
+
     }
 }
