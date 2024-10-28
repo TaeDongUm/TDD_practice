a9ac49c49d360bdf135e4fe71e6fe8719ac1537a
etd93 <etd93@naver.com>
2024-10-28 18:38:36 +0900

    test : 실패하는 테스트 작성 (Account 클래스 잔고 반환)

--- src/test/java/com/tdd/practice/domain/AccountTest.java
+++ src/test/java/com/tdd/practice/domain/AccountTest.java
@@ -1,5 +1,6 @@
 package com.tdd.practice.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -7,9 +8,15 @@ import org.junit.jupiter.api.Test;
 public class AccountTest {
 
     @Test
-    public void testAccount(){
+    public void TestAccount(){
         Account account = new Account();
         assertNotNull(account,"계좌 생성이 실패했습니다.");
 
+    }
+
+    @Test
+    public void TestAccount_Balance(){
+        Account account = new Account(10000);
+        assertEquals(10000, account.getBalance());
     }
 }
